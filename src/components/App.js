ea293d0da975a773044e74bc85af4a6bee4062f2
Madhav Milan <49267441+m2-madhav@users.noreply.github.com>
2020-11-23 17:54:48 +0530

    code Added

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,20 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
+import Home from "./Home";
+import About from "./About";
+import { Route, Switch } from "react-router-dom";
 
 class App extends Component {
-    render() {
-
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div id="main">
+        <Switch>
+          <Route path="/" component={Home} />
+          <Route path="/about" component={About} />
+        </Switch>
+      </div>
+    );
+  }
 }
-
 
 export default App;
